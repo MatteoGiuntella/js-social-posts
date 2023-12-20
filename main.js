0ee67c61b8cc2d16e95b498bdad98e89e39311c8
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-12-20 14:56:39 +0100

    stampate foto su html con js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=45"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let postScreen = ''
+
+for (let i = 0; i < posts.length; i++) {
+    
+    postScreen += ` <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src= ${posts[i]['author']['image']} alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i]['author']['name']}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text"> ${posts[i].content}</div>
+    <div class="post__image">
+        <img src=${posts[i].media} alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>`
+    
+}
+let visual = document.querySelector('#container').innerHTML = postScreen 
